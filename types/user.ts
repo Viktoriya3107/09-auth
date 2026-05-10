8bf24506badc1f8315fb332606529d5d78e1b688
Viktoriya3107 <bondarenko_vikyoriya988@ukr.net>
2026-05-10 23:23:06 +0300

    9

--- types/user.ts
+++ types/user.ts
@@ -3,16 +3,3 @@ export interface User {
   username: string;
   avatar: string;
 };
-export type Note = {
-  id: string;
-  title: string;
-  content: string;
-  tag: string;
-  createdAt?: string;
-};
-
-export type CreateNoteDTO = {
-  title: string;
-  content: string;
-  tag: string;
-};
